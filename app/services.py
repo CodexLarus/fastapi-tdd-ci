7855f861633dab2266909b14844ca566c39f4138
Brouard Simon <simon@Mac-Larus.local>
2026-02-02 16:44:28 +0100

    Break bonus logic

--- app/services.py
+++ app/services.py
@@ -5,3 +5,10 @@ def compute_bonus(sales: float) -> float:
     if sales < 50000:
         return sales * 0.05
     return sales * 0.10
+
+def compute_bonus(salary: float) -> float: 
+    if salary < 0: 
+        raise ValueError("Salary must be positive") 
+ 
+    # ERREUR VOLONTAIRE 
+    return salary * 0.2
